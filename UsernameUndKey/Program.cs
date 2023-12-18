4c6f13498dd2d8198df8d126076d4e6ed17ad2d5
murat <murat@DESKTOP-RMEFN4R>
2023-12-18 14:29:41 +0100

    Projektdateien hinzufügen.

---
+++ UsernameUndKey/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("\nUsername: ");
+string username = "";
+ConsoleKeyInfo key;
+do
+{
+    string symbol = "";
+    key = Console.ReadKey(true); 
+
+    
+    if (key.Key != ConsoleKey.Enter)
+    {
+        username += key.KeyChar;
+        symbol = "*";
+        Console.Write(symbol);
+    }
+} while (key.Key != ConsoleKey.Enter);
+
+Console.WriteLine("\nPassWort: ");
+string sifre = "";
+
+do
+{
+    string symbol = "";
+    key = Console.ReadKey(true); 
+    
+    if (key.Key != ConsoleKey.Enter)
+    {
+        sifre += key.KeyChar;
+        symbol = "*";
+        Console.Write(symbol);
+    }
+} while (key.Key != ConsoleKey.Enter);
+
